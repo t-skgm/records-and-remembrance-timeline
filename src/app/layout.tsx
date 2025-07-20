7c0e99d79a8c9a67899acd5f9be237d8794341a8
t-skgm <skgm0301@gmail.com>
2025-07-21 00:26:38 +0900

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     "ミュージシャン門田匡陽の活動年表。25年間の音楽活動を時系列で表示。",
   keywords:
     "門田匡陽, Monden MASAAKI, BURGER NUDS, Good Dog Happy Men, Poet-type.M, 年表, タイムライン",
-  authors: [{ name: "Monden MASAAKI Timeline Project" }],
+  authors: [{ name: "march-am" }],
   openGraph: {
     title: "Records and remembrance: Timeline",
     description: "ミュージシャン門田匡陽の活動年表",
